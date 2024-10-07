1a8ba0d971de8783e49c9f220680ed79051701c1
zaidturman <zaidturman17@gmail.com>
2024-10-07 18:42:18 +0300

    - just create new fils to test dashbord

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,17 +1,19 @@
 <header class="header">
-        <div class="container">
-<h1 class="headertitle">
-    نظام إدارة بنك الدم
+    <div class="container">
+        <h1 class="headertitle">
+            نظام إدارة بنك الدم
 
 
-    <i class="fas fa-tint"></i> <!-- Font Awesome blood icon -->
-</h1>           
- <nav class="navbar">
-                <a href="/">الرئيسية</a>
-                <a href="{{route('views.about')}}">عن النظام</a>
-                <a href="#">تواصل معنا</a>
-                <a href="{{ route('login') }}">تسجيل دخول</a>
+            <i class="fas fa-tint"></i> <!-- Font Awesome blood icon -->
+        </h1>
+        <nav class="navbar">
+            <a href="/">الرئيسية</a>
+            <a href="{{route('views.about')}}">عن النظام</a>
+            <a href="#">تواصل معنا</a>
+            <a href="{{ route('login') }}">تسجيل دخول</a>
+            <a href="{{ route('dashboard') }}"> dashboard</a>
 
-            </nav>
-        </div>
+
+        </nav>
+    </div>
 </header>
