094a1073e41c129b09d88f82d5e4d296b8f4a212
zaidturman <zaidturman17@gmail.com>
2024-11-21 16:10:06 +0200

    header style

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,16 +2,23 @@
     <div class="container">
         <h1 class="headertitle">
             <img src="{{ asset('images/logo.png') }}" alt="logo" class="logo">
-            <img src="{{ asset('images/logo.png') }}" alt="logo" class="logobackground">
 
             نظام إدارة بنك الدم
 
         </h1>
         <nav class="navbar">
-            <a href="/">الرئيسية</a>
-            <a href="{{route('views.about')}}">عن النظام</a>
-            <a href="{{route('views.contact')}}">تواصل معنا</a>
-            <a href="{{ route('login') }}">تسجيل دخول</a>
+            <div class="nav">
+
+                <a href="/">الرئيسية</a>
+                <a href="{{route('views.about')}}">عن النظام</a>
+                <a href="{{route('views.contact')}}">تواصل معنا</a>
+            </div>
+            <div class="login">
+
+                <a href="{{ route('login') }}" class="">تسجيل دخول</a>
+                <a href="{{ route('roles') }}" class=""> انشاء حساب</a>
+            </div>
+
 
 
         </nav>
